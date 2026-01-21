4679e91f6f89aadc630706b78feefd505cb2a1fe
(Turquoise)parukia35-afk <parukia35@gmail.com>
2026-01-22 01:45:20 +0800

    1/21 BEKUWA紀錄頁 upload
    表格輸出順序按所設定的跑、記錄尺寸已補0、標題按設定的套用

--- beetle-blog/src/pages/RecordView.vue
+++ beetle-blog/src/pages/RecordView.vue
@@ -12,37 +12,35 @@
           style="letter-spacing: 2px"
         >
           世界鍬形蟲最大紀錄2026
-
         </h1>
         <v-divider class="mx-auto mt-4" width="60" thickness="4" color="primary"></v-divider>
       </v-col>
     </v-row>
 
-    <div v-for="(group, genus) in genusClassification" :key="genus" class="mb-10 mb-md-16">
+    <div v-for="groupObj in genusClassification" :key="groupObj.genusName" class="mb-10 mb-md-16">
       <v-sheet
-        color="grey-darken-3"
-        theme="dark"
+        :style="{ backgroundColor: groupObj.bgColor, color: groupObj.textColor }"
         :class="$vuetify.display.xs ? 'pa-3' : 'pa-5'"
         class="rounded-t-xl d-flex align-center"
       >
         <v-icon
           icon="mdi-bug"
-          color="primary"
+          :style="{ color: groupObj.textColor }"
           :class="$vuetify.display.xs ? 'mr-2' : 'mr-4'"
           :size="$vuetify.display.xs ? '24' : '36'"
         ></v-icon>
         <h2 :class="$vuetify.display.xs ? 'text-h6' : 'text-h4'" class="font-weight-bold">
-          {{ genus }}
+          {{ groupObj.genusName }}
         </h2>
         <v-spacer></v-spacer>
         <v-chip
-          color="primary"
+          :color="groupObj.textColor === 'white' ? 'primary' : 'black'"
           variant="elevated"
           class="font-weight-black"
           :size="$vuetify.display.xs ? 'small' : 'large'"
         >
           <v-icon start icon="mdi-database-outline" class="d-none d-sm-inline-flex"></v-icon>
-          {{ group.length }} 筆
+          {{ groupObj.list.length }} 筆
         </v-chip>
       </v-sheet>
 
@@ -59,12 +57,12 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(item, index) in group" :key="item._id">
+            <tr v-for="(item, index) in groupObj.list" :key="item._id">
               <td
                 :class="$vuetify.display.xs ? 'text-body-1' : 'text-h6'"
                 class="font-weight-bold text-primary"
               >
-                {{ shouldShowName(group, index) ? item.commonName : '' }}
+                {{ shouldShowName(groupObj.list, index) ? item.commonName : '' }}
               </td>
 
               <td class="font-italic text-grey-darken-2 text-caption text-sm-body-2">
@@ -76,11 +74,13 @@
                   :class="$vuetify.display.xs ? 'text-body-1' : 'text-h6'"
                   class="font-weight-black text-deep-orange-darken-2"
                 >
-                  {{ item.captiveRecord }}
+                  {{ formatNumber(item.captiveRecord) }}
                 </span>
               </td>
 
-              <td class="text-center d-none d-sm-table-cell">{{ item.wildRecord || '-' }}</td>
+              <td class="text-center d-none d-sm-table-cell">
+                {{ formatNumber(item.wildRecord) || '-' }}
+              </td>
               <td class="text-body-2 text-grey-darken-1 d-none d-md-table-cell">
                 {{ item.japaneseName }}
               </td>
@@ -97,6 +97,90 @@
 import axios from 'axios'
 import { computed, onMounted, ref } from 'vue'
 
+// 紀錄尺寸補零函式
+const formatNumber = (num) => {
+  if (num === null || num === undefined || num === '') return '-'
+  return Number(num).toFixed(1)
+}
+
+const GENUS_CONFIG = [
+  {
+    name: '細身赤鍬形蟲屬 Cyclommatus',
+    bgColor: '#efbe90',
+    textColor: 'black'
+  },
+  {
+    name: '鋸鍬形蟲屬 Prosopocoilus',
+    bgColor: '#732b05',
+    textColor: 'white'
+  },
+  {
+    name: '鉗鍬屬 Kirchnerius',
+    bgColor: 'linear-gradient(135deg, #0693e3 0%, #a31c1c 36%, #9b51e0 100%)',
+    textColor: 'white'
+  },
+  {
+    name: '前鍬屬 Epidorcus',
+    bgColor: 'linear-gradient(135deg, #ba974a 0%, #a31c1c 36%, #691f1f 48%, #9b51e0 100%)',
+    textColor: 'white'
+  },
+  {
+    name: '叉角鍬形蟲屬 Hexarthrius',
+    bgColor: '#0963C4',
+    textColor: 'white'
+  },
+  {
+    name: '鹿角、金剛鹿角、偽鹿角鍬形蟲等屬 Rhaetulus、Rhaetus、Pseudorhaetus',
+    bgColor: '#14e5db',
+    textColor: 'black'
+  },
+  {
+    name: '小鍬形蟲類群(大鍬屬) Dorcus',
+    bgColor: '#111111',
+    textColor: 'white'
+  },
+  {
+    name: '大鍬(大鍬屬) Dorcus',
+    bgColor: '#111111',
+    textColor: 'white'
+  },
+  {
+    name: '扁鍬(大鍬屬) Dorcus',
+    bgColor: '#111111',
+    textColor: 'white'
+  },
+  {
+    name: '豔鍬屬 Odontolabis',
+    bgColor: '#23883D',
+    textColor: 'white'
+  },
+  {
+    name: '圓翅鍬屬 Neolucanus',
+    bgColor: '#333300',
+    textColor: 'white'
+  },
+  {
+    name: '黃金鬼屬 Allotopus',
+    bgColor: '#f2ca00',
+    textColor: 'black'
+  },
+  {
+    name: '深山屬 Lucanus',
+    bgColor: '#cf0034',
+    textColor: 'white'
+  },
+  {
+    name: '肥角屬 Aegus',
+    bgColor: '#0946e0',
+    textColor: 'white'
+  },
+  {
+    name: '其他 Others',
+    bgColor: '#7e7ce4',
+    textColor: 'white'
+  }
+]
+
 const rawBekuwaRecords = ref([])
 
 // 1. 從後端抓BekuwaRecord資料
@@ -111,22 +195,39 @@ async function fetchRecords() {
 
 // 2. 將抓到的原始資料按學名排序後依屬名分到不同的key裡
 const genusClassification = computed(() => {
-  // genusClassification 是一個依照屬名分類的函式，最終會return出一個有很多key名稱為屬名的物件。
-  const sortedBekuwaRecords = [...rawBekuwaRecords.value].sort(
-    (a, b) => a.scientificName.localeCompare(b.scientificName) // 將原始資料按學名排序
-  )
-
-  const groupedRecords = {} // 先開一個空物件
-
-  sortedBekuwaRecords.forEach((item) => {
-    if (!groupedRecords[item.genus]) {
-      groupedRecords[item.genus] = [] // 如果該物件裡沒有跟迴圈到的資料的屬名一樣的key，則創建一個以該屬名為名的key，其值為一空陣列。
-      groupedRecords[item.genus].push(item) // 然後將迴圈到的資料放進對應屬名的空陣列裡
-    } else {
-      groupedRecords[item.genus].push(item)
+  const groups = {} // 建立一個暫存物件。
+  rawBekuwaRecords.value.forEach((item) => {
+    if (!groups[item.genus]) groups[item.genus] = [] // 如果暫存陣列裡沒有一個跟迴圈到的資料的屬名同名的key，則開一個key為該屬名，其值為空陣列。
+    groups[item.genus].push(item) // 然後將迴圈到的資料 push到 跟自己的屬名相同的 key名的陣列裡。
+    // 現在，暫存物件裡，已經按屬名將資料分到對應屬名的key的陣列裡了。
+    // groups = {C屬:[{長頸鹿的資料},{孔夫子的資料}...], D屬:[{安達的資料},{長角的資料}...]}
+  })
+  const orderedResult = [] // 開一個空陣列，用來儲存依GENUS_ORDER定義的排序的結果。
+  GENUS_CONFIG.forEach((config) => {
+    if (groups[config.name]) {
+      // 如果暫存陣列裡有跟迴圈到的屬名同名的key名
+      orderedResult.push({
+        // 在orderedResult push 一個新物件，其key:genusName的值為迴圈跑到的屬名，其key:list的值是直接把暫存資料裡該屬key的值全部搬過來，並且依學名排序。
+        genusName: config.name,
+        list: groups[config.name].sort((a, b) => a.scientificName.localeCompare(b.scientificName)),
+        bgColor: config.bgColor,
+        textColor: config.textColor
+      })
+      delete groups[config.name] // 把該屬從暫存資料刪除，代表處理過了。這裡很關鍵!!是為後面的保險鋪路。
     }
   })
-  return groupedRecords
+  // orderedResult=[{genusName:C屬, list:[{學名a為首的蟲的資料},{學名b為首的蟲的資料}...]},{genusName:P屬, list:[{學名a為首的蟲的資料},{學名b為首的蟲的資料}...]}...]
+
+  Object.keys(groups).forEach((genusName) => {
+    orderedResult.push({
+      genusName: genusName,
+      list: groups[genusName].sort((a, b) => a.scientificName.localeCompare(b.scientificName)),
+      bgColor: '#424242',
+      textColor: 'white'
+    })
+  })
+
+  return orderedResult
 })
 
 // 判斷是否要顯示中文俗名
